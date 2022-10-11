7bee529d08860b77ba4ade63b62b9a10f50bba80
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-10-11 00:03:46 -0300

    resposividade para 768px/1023px do header-wrapper

--- src/app/header-wrapper/header-wrapper.component.ts
+++ src/app/header-wrapper/header-wrapper.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'headerWrapper',
   templateUrl: './header-wrapper.component.html',
-  styleUrls: ['./header-wrapper.component.css'],
+  styleUrls: ['./header-wrapper.component.css', './header-wrapper.responsive.component.css'],
 })
 export class HeaderWrapper {
   @Input() name: string;
