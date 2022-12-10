e4f0dfb26726a5a218c010a9e8ef6f92e04fdbd1
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-12-10 18:00:41 -0300

    reiniciando o menu carousel

--- src/app/carousel-menu/carousel-menu.component.ts
+++ src/app/carousel-menu/carousel-menu.component.ts
@@ -1,22 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-carousel-menu',
   templateUrl: './carousel-menu.component.html',
-  styleUrls: ['./carousel-menu.component.css', './carousel-menu-responsive.component.css']
-})
-export class CarouselMenuComponent implements OnInit {
+  styleUrls: ['./carousel-menu.component.css', './carousel-menu-responsive.component.css'],
 
-  constructor() { }
+})
+
+export class CarouselMenuComponent implements OnInit {
+  count: number = 0;
+
+  constructor() {}
 
   ScrolltoLeft() {
-    document.getElementById('carrousel').scrollBy(-5,0);
+
+    if(this.count == 0 ) {
+      this.count = 6;
+    } else {
+      this.count = this.count - 1;
+      console.log("scroll para esquerda");
+    }
+
+    console.log(this.count);
   }
   ScrolltoRight() {
-    document.getElementById('carrousel').scrollBy(30,0);
+
+    if(this.count == 6) {
+      this.count = 0;
+    } else {
+      this.count = this.count + 1;
+      console.log("scroll para direita");
+    }
+    console.log(this.count);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
